2e0b20a50745a8ccb442b48ed6628e63ab622864
ValueAchooMatthew <78985742+ValueAchooMatthew@users.noreply.github.com>
2024-01-16 23:33:42 -0500

    Worked on backend using firebase

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,10 +1,27 @@
 "use client";
 
-import { BaseSyntheticEvent } from "react"
+import { initializeApp } from "firebase/app";
+import { doc, getFirestore, updateDoc } from "firebase/firestore"; 
+import {collection, getDocs} from "firebase/firestore";
+import Link from "next/link";
+import { BaseSyntheticEvent, useEffect, useState } from "react"
 
-export default function Home(props: any) {
-  
-  
+const firebaseConfig = {
+  apiKey: "AIzaSyDTvp8xuIL6CHzciwAgQL2I7jCWkR_HmwI",
+  authDomain: "chaotic-url-shortener.firebaseapp.com",
+  databaseURL: "https://chaotic-url-shortener-default-rtdb.firebaseio.com",
+  projectId: "chaotic-url-shortener",
+  storageBucket: "chaotic-url-shortener.appspot.com",
+  messagingSenderId: "489084798653",
+  appId: "1:489084798653:web:1ee9361cd24591d84c99d4",
+  measurementId: "G-7K5L05Z6LW"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+export default function Home() {
+
+  const [links, setLinks] = useState<{original: string, shortened: string}[]>()
 
   const createURL = (website: string): URL | null => {
     try{
@@ -16,20 +33,32 @@ export default function Home(props: any) {
     }
   }
 
-  const handleSubmit = (event: BaseSyntheticEvent)=>{
+  const handleSubmit = async (event: BaseSyntheticEvent)=>{
     event.preventDefault();
     if(event.target instanceof HTMLFormElement){
       if(event.target[0] instanceof HTMLInputElement){
         const submitted_text: string = event.target[0].value;
         const response = createURL(submitted_text);
         if(response){
-          console.log(response)
+          const snapshot = doc(db, "websites/shortened sites");
+          console.log(snapshot);
+          const time =`${new Date().getTime()}`;
+          const submit = {
+            [time]: `${response}`
+          }
+          updateDoc(snapshot, submit)
+          setLinks((prevLinks) => {
+            if(prevLinks){
+              return [...prevLinks, {original: submit[time], shortened: time} ];
+            }else{
+              return [{original: submit[time], shortened: time}];
+            }
+          });
         }
       }
     }
   }
   return (
-
     <main className="p-14 ">
       <h1 className="text-center text-4xl font-semibold">
         Chaotic Url Generator
@@ -39,6 +68,28 @@ export default function Home(props: any) {
           <input className="w-full h-full rounded-lg text-3xl px-3" type="text" />
         </form>
       </div>
+      <div className="text-center mt-12">
+        {links?
+          <div className="">
+            {
+            links.map((link, index)=>{
+              return(
+                <div className="flex text-3xl justify-center" key={index}>
+                  <Link className="inline-block" href={link.original}>
+                    {link.original}:    
+                  </Link>
+                  <Link className="inline-block font-semibold" href={link.original}>
+                    {link.shortened}
+                  </Link>
+                </div>
+              )})
+            }
+          </div>
+          :
+          null
+        }
+      </div>
+
     </main>
   )
 }
---
+++ app/api/test/route.ts
@@ -0,0 +1,13 @@
+// import { NextResponse } from "next/server"
+// import { initializeApp } from "firebase/app";
+// import { collection, addDoc, getFirestore } from "firebase/firestore";
+// import firebase from "firebase/compat/app";
+
+
+
+// // const app = initializeApp(firebaseConfig);
+// export async function GET(request: Request) {
+//     const ballsack = new URL(request.url)
+//     console.log(ballsack)  
+//     return NextResponse.json({"Hi!": ":)"})
+// }
